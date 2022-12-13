5ec3c8aff1342545b6550f79837284fa225b5805
Maksym Drobot <drobotm.com@gmail.com>
2022-12-13 16:22:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <div>
+        <div>
+          <p>React</p>
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
